04248dd161775870918b30ae577ddd0b4de03fbf
tonylepage <tony@lep.ag>
2022-09-28 13:31:38 +0800

    sync with macbook

---
+++ ChainBeacon/ChainBeaconApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChainBeaconApp.swift
+//  ChainBeacon
+//
+//  Created by Lepage Tony on 2022/9/28.
+//
+
+import SwiftUI
+
+@main
+struct ChainBeaconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
